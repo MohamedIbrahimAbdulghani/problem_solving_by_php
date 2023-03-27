ac4b08daadc1a2e776e07b9042a6f8b08e74967e
Mohamed Ibrahim <mohamedibrahimabdulghani@gmail.com>
2023-03-28 01:35:12 +0200

    2

--- 2.php
+++ 2.php
@@ -3,3 +3,27 @@
 // reverse this string without any built in function in php
 
 
+
+function reverseString($string) {
+    $i = strlen($string) - 1;
+    $revStr = "";
+    while($i >= 0):
+        $revStr .= $string[$i];
+        $i--;
+    endwhile;
+    return $revStr;
+}
+
+
+echo reverseString("alahly");
+
+
+// $string = "mohamed";
+
+// $i = 0;
+// $length = strlen($string);
+
+// while($i < $length):
+//     echo $string[$i] . "<br>";
+//     $i++;
+// endwhile;
